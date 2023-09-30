dc5696c90fd7965eb5f2ebfbf5c6a5e30c849e66
Doni Wahyu <doniwyk@gmail.com>
2023-09-30 15:53:07 +0700

    Delete oop-java/week-05 directory

--- oop-java/week-05/src/LabStaff.java
+++
@@ -1,5 +0,0 @@
-public class LabStaff extends Person{
-    public LabStaff(String id, String name, int age, String gender) {
-        super(id, name, age, gender);
-    }
-}
--- oop-java/week-05/src/Nurse.java
+++
@@ -1,5 +0,0 @@
-public class Nurse extends Person{
-    public Nurse(String id, String name, int age, String gender) {
-        super(id, name, age, gender);
-    }
-}
