af4a94cb5e7cf1f374000f4890e85f9af0d6cb91
Doniwyk <doniwyk@gmail.com>
2023-10-11 02:17:09 +0700

    add week-5 assignments

--- basic-web/week-05/string1.php
+++
@@ -1,9 +0,0 @@
-<?php
-$loremipsum ="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia quod vitae dolorem? Illum inventore tenetur, repellendus obcaecati sed esse placeat omnis ipsa delectus quam blanditiis rem cupiditate quibusdam animi. Quisquam!";
-
-echo "<p>$loremipsum</p>";
-echo "String length: ".strlen($loremipsum)."<br/>";
-echo "String word count: ".str_word_count($loremipsum)."<br/>";
-echo "<p>" . strtoupper($loremipsum) . "</p>";
-echo "<p>" . strtolower($loremipsum) . "</p>";
-?>
